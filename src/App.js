b0ce33371553403cac7520047b6d5250a3b130c7
dehemi weerakoon <115975647+dehemiweerakoon@users.noreply.github.com>
2024-08-13 16:13:49 +0530

    added new developemts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import Shop from './pages/Shop';
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import LoginSignup from './pages/LoginSignup';
 function App() {
   return (
     <div>
-      <NavBar/>
+     
+      <BrowserRouter>
+       <NavBar/>
+       <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="mens"/>}/>
+        <Route path='/woman' element={<ShopCategory category="woman"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavBar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 function NavBar() {
+  const [menu,setMenu] = useState("shop")
   return (
     <div className='navbar'>
-
+      <div className='nav-logo'>
+        <img src={logo} alt=''/>
+        <p>SHOPPER</p>
+      </div>
+      <ul className='nav-menu'>
+        <li onClick={()=>{setMenu("shop")}}><Link to={'/'} style={{textDecoration:'none'}}>Shop</Link> {menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("men")}}><Link to={'/mens'}style={{textDecoration:'none'}}>Men</Link>{menu==="men"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("woman")}}><Link to={'/woman'}style={{textDecoration:'none'}}>Woman</Link>{menu==="woman"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}><Link to={'/kids'} style={{textDecoration:'none'}}>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to='/login'><button>Log In</button></Link>
+        <Link to='/cart'><img src={cart_icon} alt=""/></Link>
+        <div className="nav-cart-count">
+          <p>0</p>
+        </div>
+      </div>
     </div>
   )
 }
